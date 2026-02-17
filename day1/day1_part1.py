4e98a4412d35acc191c5ca32f5c6470fe14094af
hanner-r <hrperry01@gmail.com>
2026-02-17 16:41:06 +0000

    got rid of unnecessary code in is_dial_position_zero function. improved d1p1 code complete :)

--- day1/day1_part1.py
+++ day1/day1_part1.py
@@ -10,6 +10,7 @@ dial_at_zero_count = 0
 def is_dial_position_zero(starting_position, dial_rotation):
     rotation_direction = dial_rotation[0]
     rotation_distance = int(dial_rotation[1:])
+
     while rotation_distance > 99:
         rotation_distance -= 100
 
@@ -22,19 +23,6 @@ def is_dial_position_zero(starting_position, dial_rotation):
         new_position -= 100
     elif new_position < 0:
         new_position += 100
-
-    # if rotation_direction == 'R':  # how can I change this loop to remove repetition?
-    #     new_position = starting_position + rotation_distance
-    #     if new_position > 99:
-    #         new_position -= 100
-    #     elif new_position < 0:
-    #         new_position += 100
-    # else:
-    #     new_position = starting_position - rotation_distance
-    #     if new_position > 99:
-    #         new_position -= 100
-    #     elif new_position < 0:
-    #         new_position += 100
 
     return (new_position == 0), new_position
 
