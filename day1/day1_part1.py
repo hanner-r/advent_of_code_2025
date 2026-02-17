8a1ebff5138df0a239b390b4410fdc6a1194d4ff
hanner-r <hrperry01@gmail.com>
2026-02-17 15:54:16 +0000

    created is_dial_position_zero function

--- day1/day1_part1.py
+++ day1/day1_part1.py
@@ -5,3 +5,8 @@ with open('day1_input.txt', 'r') as input_file:
     rotations_list = [line.replace('\n', '') for line in input_file.readlines()]
 
 print(rotations_list)
+
+dial_start_position = 50
+
+def is_dial_position_zero(starting_position, rotation):
+    # something recursive?
