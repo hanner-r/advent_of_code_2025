6df2b5dd34b777ae6f0f8d5129a87cb8fea000e0
hanner-r <hrperry01@gmail.com>
2026-02-17 17:46:04 +0000

    started to look at day 1 part 2 and add code to count dial passing zero - needs work

---
+++ day1/day1_part2.py
@@ -0,0 +1,40 @@
+with open('day1_input.txt', 'r') as input_file:
+    rotations_list = [line.replace('\n', '') for line in input_file.readlines()]
+
+dial_start_position = 50
+dial_at_zero_count = 0
+dial_passes_zero_count = 0
+
+
+def is_dial_position_zero(starting_position, dial_rotation, count):
+    rotation_direction = dial_rotation[0]
+    rotation_distance = int(dial_rotation[1:])
+
+    while rotation_distance > 99:
+        rotation_distance -= 100
+        count += 1
+
+    if rotation_direction == 'L':
+        rotation_distance = -1 * rotation_distance
+
+    new_position = starting_position + rotation_distance
+
+    if new_position > 99:
+        new_position -= 100
+        count += 1
+    elif new_position < 0:
+        new_position += 100
+        count += 1
+
+    return (new_position == 0), new_position, count
+
+
+for rotation in rotations_list:
+    result = is_dial_position_zero(dial_start_position, rotation, dial_passes_zero_count)
+    if result[0] is True:
+        dial_at_zero_count += 1
+    dial_start_position = result[1]
+    dial_passes_zero_count += result[2]
+
+print(f'The password is {dial_passes_zero_count + dial_at_zero_count}!')
+# getting a very long number as the answer so something is definitely not right!
