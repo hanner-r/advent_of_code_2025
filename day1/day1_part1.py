82ed5b7549f25682fb6be6980a9f522ae640e519
hanner-r <hrperry01@gmail.com>
2026-02-17 16:25:56 +0000

    added to function to account for 3-digit rotations

--- day1/day1_part1.py
+++ day1/day1_part1.py
@@ -11,15 +11,18 @@ dial_at_zero_count = 0
 
 
 def is_dial_position_zero(starting_position, dial_rotation):
+    rotation_distance = int(dial_rotation[1:])
+    while rotation_distance > 99:
+        rotation_distance -= 100
 
     if dial_rotation[0] == 'R':
-        new_position = starting_position + int(dial_rotation[1:])
+        new_position = starting_position + rotation_distance
         if new_position > 99:
             new_position -= 100
         elif new_position < 0:
             new_position += 100
     else:
-        new_position = starting_position - int(dial_rotation[1:])
+        new_position = starting_position - rotation_distance
         if new_position > 99:
             new_position -= 100
         elif new_position < 0:
