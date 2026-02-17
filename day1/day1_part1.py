abbca0228ad6bf10037f7f3979e468085a51afae
hanner-r <hrperry01@gmail.com>
2026-02-17 15:50:53 +0000

    read info from input file into a list

---
+++ day1/day1_part1.py
@@ -0,0 +1,7 @@
+# The actual password is the number of times the dial is left pointing at 0 after
+# any rotation in the sequence
+
+with open('day1_input.txt', 'r') as input_file:
+    rotations_list = [line.replace('\n', '') for line in input_file.readlines()]
+
+print(rotations_list)
