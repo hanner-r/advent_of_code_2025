6a1cacd6a2bac8c64c03545c9d2e54f47b2a6938
hanner-r <hrperry01@gmail.com>
2026-02-17 16:34:46 +0000

    created variable rotation_direction (which == dial_rotation[0]) for readability

--- day1/day1_part1.py
+++ day1/day1_part1.py
@@ -8,11 +8,12 @@ dial_at_zero_count = 0
 
 
 def is_dial_position_zero(starting_position, dial_rotation):
+    rotation_direction = dial_rotation[0]
     rotation_distance = int(dial_rotation[1:])
     while rotation_distance > 99:
         rotation_distance -= 100
 
-    if dial_rotation[0] == 'R':
+    if rotation_direction == 'R':
         new_position = starting_position + rotation_distance
         if new_position > 99:
             new_position -= 100
