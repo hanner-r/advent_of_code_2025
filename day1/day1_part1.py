fc1083cdec515d1a05fe80b39cd7a7e3835df3fc
hanner-r <hrperry01@gmail.com>
2026-02-17 16:23:25 +0000

    added notes about 3-digit rotation numbers needing to be considered in the logic

--- day1/day1_part1.py
+++ day1/day1_part1.py
@@ -10,15 +10,16 @@ dial_start_position = 50
 dial_at_zero_count = 0
 
 
-def is_dial_position_zero(starting_position, rotation):
-    if rotation[0] == 'R':
-        new_position = starting_position + int(rotation[1:])
+def is_dial_position_zero(starting_position, dial_rotation):
+
+    if dial_rotation[0] == 'R':
+        new_position = starting_position + int(dial_rotation[1:])
         if new_position > 99:
             new_position -= 100
         elif new_position < 0:
             new_position += 100
     else:
-        new_position = starting_position - int(rotation[1:])
+        new_position = starting_position - int(dial_rotation[1:])
         if new_position > 99:
             new_position -= 100
         elif new_position < 0:
@@ -33,3 +34,6 @@ for rotation in rotations_list:
         dial_at_zero_count += 1
     dial_start_position = result[1]
 
+print(dial_at_zero_count)
+# I got an answer of 430, which is too low. need to review my logic
+# I see now that some rotations are 3 digits (e.g. 'L692') - I need to account for these.
